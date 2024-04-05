f3e77af4db73d884425c7d1148333298d5056cea
SaatvatG <saatvatg@gmail.com>
2024-04-06 00:42:00 +0530

    created user and post models

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             // To avoid warnings in the console
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
 
         console.log(`MongoDB Connected ${conn.connection.host}`);

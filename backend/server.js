15ccd13a0249f0cc84c1f50e2fdcbd52b1ce653c
SaatvatG <saatvatg@gmail.com>
2024-04-05 23:22:57 +0530

    added connection to the database

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () =>
+    console.log(`Server started at http://localhost:${PORT}`)
+);

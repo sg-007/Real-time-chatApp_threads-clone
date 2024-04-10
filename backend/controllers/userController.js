4e56104dc78829c3911034ed5136ec0860a50541
SaatvatG <saatvatg@gmail.com>
2024-04-10 17:35:07 +0530

    update profile functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenSetCookie from "../utils/helpers/generateTokenSetCookies.js";
+import { v2 as cloudinary } from "cloudinary";
 
 const getUserProfile = async (req, res) => {
     const { username } = req.params;
@@ -43,6 +44,8 @@ const signupUser = async (req, res) => {
                 name: newUser.name,
                 email: newUser.email,
                 username: newUser.username,
+                bio: newUser.bio,
+                profilePic: newUser.profilePic,
             });
         } else {
             res.status(400).json({ error: "Invalid user data" });
@@ -74,6 +77,8 @@ const loginUser = async (req, res) => {
             name: user.name,
             email: user.email,
             username: user.username,
+            bio: user.bio,
+            profilePic: user.profilePic,
         });
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -136,7 +141,8 @@ const followUnfollowUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    const { name, email, username, password, profilePic, bio } = req.body;
+    const { name, email, username, password, bio } = req.body;
+    let { profilePic } = req.body;
     const userId = req.user._id;
     try {
         let user = await User.findById(userId);
@@ -153,6 +159,18 @@ const updateUser = async (req, res) => {
             user.password = hashedPassword;
         }
 
+        if (profilePic) {
+            if (user.profilePic) {
+                await cloudinary.uploader.destroy(
+                    user.profilePic.split("/").pop().split(".")[0]
+                );
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(
+                profilePic
+            );
+            profilePic = uploadedResponse.secure_url;
+        }
+
         user.name = name || user.name;
         user.email = email || user.email;
         user.username = username || user.username;
@@ -161,7 +179,9 @@ const updateUser = async (req, res) => {
 
         user = await user.save();
 
-        res.status(200).json({ message: "Profile updated successfully", user });
+        user.password = null;
+
+        res.status(200).json(user);
     } catch (err) {
         res.status(500).json({ error: err.message });
         console.log("Error in updateUser: ", err.message);
--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -13,108 +13,181 @@ import {
 import { useRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import { useState } from "react";
+import { useRef } from "react";
+import usePreviewImg from "../hooks/usePreviewImg";
+import useShowToast from "../hooks/useShowToast";
 
 export default function UpdateProfilePage() {
     const [user, setUser] = useRecoilState(userAtom);
     const [inputs, setInputs] = useState({
-        name: "",
-        username: "",
-        email: "",
-        bio: "",
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
         password: "",
     });
-    console.log(user, "user is here");
+
+    const fileRef = useRef(null);
+    const showToast = useShowToast();
+
+    const { handleImageChange, imgUrl } = usePreviewImg();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch(`/api/users/update/${user._id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ ...inputs, profilePic: imgUrl }),
+            });
+            const data = await res.json(); // updated user object
+            if (data.error) {
+                showToast("Error", data.error, "error");
+                return;
+            }
+            showToast("Success", "Profile updated successfully", "success");
+            setUser(data);
+            localStorage.setItem("user-threads", JSON.stringify(data));
+        } catch (error) {
+            showToast("Error", error, "error");
+        }
+    };
     return (
-        <Flex align={"center"} justify={"center"} my={6}>
-            <Stack
-                spacing={4}
-                w={"full"}
-                maxW={"md"}
-                bg={useColorModeValue("white", "gray.dark")}
-                rounded={"xl"}
-                boxShadow={"lg"}
-                p={6}
-                my={12}
-            >
-                <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
-                    Update Profile
-                </Heading>
-                <FormControl id="userName">
-                    <Stack direction={["column", "row"]} spacing={6}>
-                        <Center>
-                            <Avatar
-                                size="xl"
-                                src="https://bit.ly/sage-adebayo"
-                            />
-                        </Center>
-                        <Center w="full">
-                            <Button w="full">Change Avatar</Button>
-                        </Center>
+        <form onSubmit={handleSubmit}>
+            <Flex align={"center"} justify={"center"} my={6}>
+                <Stack
+                    spacing={4}
+                    w={"full"}
+                    maxW={"md"}
+                    bg={useColorModeValue("white", "gray.dark")}
+                    rounded={"xl"}
+                    boxShadow={"lg"}
+                    p={6}
+                    my={12}
+                >
+                    <Heading
+                        lineHeight={1.1}
+                        fontSize={{ base: "2xl", sm: "3xl" }}
+                    >
+                        Update Profile
+                    </Heading>
+                    <FormControl id="userName">
+                        <Stack direction={["column", "row"]} spacing={6}>
+                            <Center>
+                                <Avatar
+                                    size="xl"
+                                    boxShadow={"md"}
+                                    src={imgUrl || user.profilePic}
+                                />
+                            </Center>
+                            <Center w="full">
+                                <Button
+                                    w="full"
+                                    onClick={() => fileRef.current.click()}
+                                >
+                                    Change Avatar
+                                </Button>
+                                <Input
+                                    type="file"
+                                    hidden
+                                    ref={fileRef}
+                                    onChange={handleImageChange}
+                                />
+                            </Center>
+                        </Stack>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Full name</FormLabel>
+                        <Input
+                            placeholder="John Doe"
+                            value={inputs.name}
+                            onChange={(e) =>
+                                setInputs({ ...inputs, name: e.target.value })
+                            }
+                            _placeholder={{ color: "gray.500" }}
+                            type="text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>User name</FormLabel>
+                        <Input
+                            placeholder="johndoe"
+                            value={inputs.username}
+                            onChange={(e) =>
+                                setInputs({
+                                    ...inputs,
+                                    username: e.target.value,
+                                })
+                            }
+                            _placeholder={{ color: "gray.500" }}
+                            type="text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Email address</FormLabel>
+                        <Input
+                            value={inputs.email}
+                            onChange={(e) =>
+                                setInputs({ ...inputs, email: e.target.value })
+                            }
+                            placeholder="your-email@example.com"
+                            _placeholder={{ color: "gray.500" }}
+                            type="email"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Bio</FormLabel>
+                        <Input
+                            value={inputs.bio}
+                            onChange={(e) =>
+                                setInputs({ ...inputs, bio: e.target.value })
+                            }
+                            placeholder="Your Bio"
+                            _placeholder={{ color: "gray.500" }}
+                            type="text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            value={inputs.password}
+                            onChange={(e) =>
+                                setInputs({
+                                    ...inputs,
+                                    password: e.target.value,
+                                })
+                            }
+                            placeholder="password"
+                            _placeholder={{ color: "gray.500" }}
+                            type="password"
+                        />
+                    </FormControl>
+                    <Stack spacing={6} direction={["column", "row"]}>
+                        <Button
+                            bg={"red.400"}
+                            color={"white"}
+                            w="full"
+                            _hover={{
+                                bg: "red.500",
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            bg={"green.400"}
+                            color={"white"}
+                            w="full"
+                            _hover={{
+                                bg: "green.500",
+                            }}
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
                     </Stack>
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Full name</FormLabel>
-                    <Input
-                        placeholder="John Doe"
-                        _placeholder={{ color: "gray.500" }}
-                        type="text"
-                    />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>User name</FormLabel>
-                    <Input
-                        placeholder="johndoe"
-                        _placeholder={{ color: "gray.500" }}
-                        type="text"
-                    />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Email address</FormLabel>
-                    <Input
-                        placeholder="your-email@example.com"
-                        _placeholder={{ color: "gray.500" }}
-                        type="email"
-                    />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Bio</FormLabel>
-                    <Input
-                        placeholder="Your Bio"
-                        _placeholder={{ color: "gray.500" }}
-                        type="email"
-                    />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                        placeholder="password"
-                        _placeholder={{ color: "gray.500" }}
-                        type="password"
-                    />
-                </FormControl>
-                <Stack spacing={6} direction={["column", "row"]}>
-                    <Button
-                        bg={"red.400"}
-                        color={"white"}
-                        w="full"
-                        _hover={{
-                            bg: "red.500",
-                        }}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        bg={"green.400"}
-                        color={"white"}
-                        w="full"
-                        _hover={{
-                            bg: "green.500",
-                        }}
-                    >
-                        Submit
-                    </Button>
                 </Stack>
-            </Stack>
-        </Flex>
+            </Flex>
+        </form>
     );
 }

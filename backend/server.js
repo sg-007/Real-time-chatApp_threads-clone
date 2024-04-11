037f47d510886da037042e6d18f5e2d5869f8de9
SaatvatG <saatvatg@gmail.com>
2024-04-12 00:29:06 +0530

    increased limit to post images upto size of 50mbw

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ cloudinary.config({
 });
 
 //middlewares
-app.use(express.json()); //To parse JSON data in the req.body
+app.use(express.json({limit:"50mb"})); //To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); //To parse form data in the req.body
 app.use(cookieParser());
 

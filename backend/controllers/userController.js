0a9d46cf68383045a17d449c600b8e0d5fd67217
SaatvatG <saatvatg@gmail.com>
2024-04-12 20:37:24 +0530

    updated getUserProfile method to take either username or userId as query

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,25 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenSetCookie from "../utils/helpers/generateTokenSetCookies.js";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
 const getUserProfile = async (req, res) => {
-    const { username } = req.params;
+    // will be userProfile either by userId or username
+    // query is either username or password
+    const { query } = req.params;
+
     try {
-        const user = await User.findOne({ username })
-            .select("-password")
-            .select("-updatedAt");
+        let user;
+        if (mongoose.Types.ObjectId.isValid(query)) {
+            user = await User.findOne({ _id: query })
+                .select("-password")
+                .select("-updatedAt");
+        } else {
+            user = await User.findOne({ username: query })
+                .select("-password")
+                .select("-updatedAt");
+        }
+
         if (!user) return res.status(404).json({ error: "User not found" });
         res.status(200).json(user);
     } catch (err) {

62e0cb5031888535b188fe66c9252a0d4dd44dc1
SaatvatG <saatvatg@gmail.com>
2024-04-12 20:42:25 +0530

    added home and profile icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,40 @@
-import { Flex, Image, useColorMode } from '@chakra-ui/react'
+import { Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai"
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <Flex justifyContent={"center"} mt={6} mb="12">
-    <Image 
-    cursor={"pointer"}
-    alt='logo'
-    w={6}
-    src={colorMode === 'dark' ? "/light-logo.svg" : "/dark-logo.svg"}
-    onClick={toggleColorMode}
-    />
-    </Flex>
-  )
-}
+    const user = useRecoilValue(userAtom);
 
-export default Header
+    return (
+        <Flex justifyContent={"space-between"} mt={6} mb="12">
+            {user && (
+                <Link as={RouterLink} to="/">
+                    <AiFillHome size={24} />
+                </Link>
+            )}
+
+            <Image
+                cursor={"pointer"}
+                alt="logo"
+                w={6}
+                src={
+                    colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"
+                }
+                onClick={toggleColorMode}
+            />
+
+            {user && (
+                <Link as={RouterLink} to={`/${user.username}`}>
+                    <RxAvatar size={24} />
+                </Link> 
+            )}
+
+        </Flex>
+    );
+};
+
+export default Header;

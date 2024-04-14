fa15db90ee360c61dc7dead9378d18288f6739cb
SaatvatG <saatvatg@gmail.com>
2024-04-14 21:02:56 +0530

    updated deletePost function to delete img from cloud

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -53,7 +53,7 @@ const getPost = async (req, res) => {
         if (!post) {
             return res.status(404).json({ error: "Post not found" });
         }
-        res.status(200).json({ post });
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -71,7 +71,10 @@ const deletePost = async (req, res) => {
                 .status(401)
                 .json({ error: "Unauthorized to delete post" });
         }
-
+        if (post.img) {
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
         await Post.findByIdAndDelete(req.params.id);
         res.status(200).json({ message: "Post deleted successfully" });
     } catch (err) {
@@ -146,6 +149,19 @@ const getFeedPosts = async (req, res) => {
     }
 };
 
+const getUserPosts = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({ username });
+        if (!user) return res.status(404).json({ error: "User not found" });
+        const posts = await Post.find({ postedBy: user._id }).sort({
+            createdAt: -1,
+        });
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 export {
     createPost,
     getPost,
@@ -153,4 +169,5 @@ export {
     likeUnLikePost,
     replyToPost,
     getFeedPosts,
+    getUserPosts,
 };

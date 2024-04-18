ee583a215224fb116c7d5c0e97a042cd979cfde4
SaatvatG <saatvatg@gmail.com>
2024-04-18 16:14:46 +0530

    formatting and created hook for getting user profile

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,14 +6,20 @@ const Comment = ({ reply, lastReply }) => {
             <Flex gap={4} py={2} my={2} w={"full"}>
                 <Avatar src={reply.userProfilePic} size={"sm"} />
                 <Flex gap={1} w={"full"} flexDirection={"column"}>
-                    <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-                        <Text fontSize={"sm"} fontWeight={"bold"}>{reply.username}</Text>
+                    <Flex
+                        w={"full"}
+                        justifyContent={"space-between"}
+                        alignItems={"center"}
+                    >
+                        <Text fontSize={"sm"} fontWeight={"bold"}>
+                            {reply.username}
+                        </Text>
                     </Flex>
                     <Text>{reply.text}</Text>
                 </Flex>
             </Flex>
 
-            {!lastReply ? < Divider /> : null}
+            {!lastReply ? <Divider /> : null}
         </>
     );
 };

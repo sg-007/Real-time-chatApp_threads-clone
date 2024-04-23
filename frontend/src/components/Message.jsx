59c27227c5c2fb9322c523f9cdd3483cff37eb1c
SaatvatG <saatvatg@gmail.com>
2024-04-24 00:59:37 +0530

    added modal for image input from user instead of text

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import { selectedConversationAtom } from "../atoms/messagesAtom";
 import userAtom from "../atoms/userAtom";
@@ -12,24 +12,47 @@ const Message = ({ ownMessage, message }) => {
         <>
             {ownMessage ? (
                 <Flex gap={2} alignSelf={"flex-end"}>
-                    <Flex bg={"green.800"} maxW={"350px"} p={1} borderRadius={"md"}>
-                        <Text color={"white"}>{message.text}</Text>
-                        <Box
-                            alignSelf={"flex-end"}
-                            ml={1}
-                            color={message.seen ? "blue.400" : ""}
-                            fontWeight={"bold"}
-                        >
-                            <BsCheck2All size={16} />
-                        </Box>
-                    </Flex>
+                    {false && (
+                        <Flex bg={"green.800"} maxW={"350px"} p={1} borderRadius={"md"}>
+                            <Text color={"white"}>{message.text}</Text>
+                            <Box
+                                alignSelf={"flex-end"}
+                                ml={1}
+                                color={message.seen ? "blue.400" : ""}
+                                fontWeight={"bold"}
+                            >
+                                <BsCheck2All size={16} />
+                            </Box>
+                        </Flex>
+                    )}
+                    {true && (
+                        <Flex mt={0} w={"200px"}>
+                            <Image
+                                src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=1800&t=st=1713898495~exp=1713899095~hmac=be0df389ee9da1836f3b9f7059f1306524f96f47ecc99182448388504e15c489"
+                                alt="Message Img"
+                                borderRadius={4}
+                            />
+                        </Flex>
+                    )}
+                    <Avatar src={user.profilePic} w={7} h={7} />
                 </Flex>
             ) : (
                 <Flex gap={2}>
                     <Avatar src={selectedConversation.userProfilePic} w={7} h={7} />
-                    <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"} color={"black"}>
-                        {message.text}
-                    </Text>
+                    {message.text && (
+                        <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"} color={"black"}>
+                            {message.text}
+                        </Text>
+                    )}
+                    {message.img && (
+                        <Flex mt={5} w={"200px"}>
+                            <Image
+                                src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=1800&t=st=1713898495~exp=1713899095~hmac=be0df389ee9da1836f3b9f7059f1306524f96f47ecc99182448388504e15c489"
+                                alt="Message Img"
+                                borderRadius={4}
+                            />
+                        </Flex>
+                    )}
                 </Flex>
             )}
         </>
--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,15 +1,33 @@
-import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+    Flex,
+    Image,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { IoSendSharp } from "react-icons/io5";
 import useShowToast from "../hooks/useShowToast";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom";
+import { BsFillImageFill } from "react-icons/bs";
+import usePreviewImg from "../hooks/usePreviewImg";
 
 const MessageInput = ({ setMessages }) => {
     const [messageText, setMessageText] = useState("");
     const showToast = useShowToast();
     const selectedConversation = useRecoilValue(selectedConversationAtom);
     const setConversations = useSetRecoilState(conversationsAtom);
+    const imageRef = useRef(null);
+    const { onClose } = useDisclosure();
+    const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
 
     const handleSendMessage = async (e) => {
         e.preventDefault();
@@ -55,19 +73,46 @@ const MessageInput = ({ setMessages }) => {
     };
 
     return (
-        <form onSubmit={handleSendMessage}>
-            <InputGroup>
-                <Input
-                    w={"full"}
-                    placeholder="Type a message"
-                    onChange={(e) => setMessageText(e.target.value)}
-                    value={messageText}
-                />
-                <InputRightElement onClick={handleSendMessage}>
-                    <IoSendSharp />
-                </InputRightElement>
-            </InputGroup>
-        </form>
+        <Flex gap={2} alignItems={"center"}>
+            <form onSubmit={handleSendMessage} style={{ flex: 95 }}>
+                <InputGroup>
+                    <Input
+                        w={"full"}
+                        placeholder="Type a message"
+                        onChange={(e) => setMessageText(e.target.value)}
+                        value={messageText}
+                    />
+                    <InputRightElement onClick={handleSendMessage}>
+                        <IoSendSharp />
+                    </InputRightElement>
+                </InputGroup>
+            </form>
+            <Flex flex={5} cursor={"pointer"}>
+                <BsFillImageFill size={20} onClick={() => imageRef.current.click()} />
+                <Input type={"file"} hidden ref={imageRef} onChange={handleImageChange} />
+            </Flex>
+            <Modal
+                isOpen={imgUrl}
+                onClose={() => {
+                    onClose();
+                    setImgUrl("");
+                }}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader></ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Flex mt={5} w={"full"}>
+                            <Image src={imgUrl} />
+                        </Flex>
+                        <Flex justifyContent={"flex-end"} my={2}>
+                            <IoSendSharp size={24} cursor={"pointer"} />
+                        </Flex>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </Flex>
     );
 };
 

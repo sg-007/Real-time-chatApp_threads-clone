17ea868990669d780d991c6169f51d26085f7bfc
SaatvatG <saatvatg@gmail.com>
2024-04-25 14:25:33 +0530

    deployment added

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,14 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 
 connectDB();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -28,5 +32,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5001;
+// http://localhost:3001 => backend/frontend
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
+
+//react app
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
